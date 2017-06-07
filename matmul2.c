2d62c351fcde19ce2d2dd96e7505476909c49f6c
Nicole Butler <nbutler3112@gmail.com>
2017-06-07 14:03:26 -0700

    Messing around to see what the best way to implement cache is

--- matmul2.c
+++ matmul2.c
@@ -1,25 +1,38 @@
 /* memory management, code density, Cache emulation - statistics generation */
 /* Generated for CSC 315 Lab 5 */
 #define AMAX 10			/* Maximum (square) array size */
-#define CACHESIM 0		/* Set to 1 if simulating Cache */
+#define CACHESIM 1		/* Set to 1 if simulating Cache */
+#define CACHESIZE 16    /* Set to cache size (16 or 256) */
+#define ASSOC 1         /* Set to associativity option (1, 2, or 4) */
+#define BOLEN 2         /* Set to log2(sizeof(int)) = 2*/
+#define INDEXLEN 6      /* Set to log2(CACHESIZE) = 6 or 10 */
+#define TAGLEN 56       /* Set to server size (64) - BOLEN - INDEXLEN = 56 or 52*/
 #include <stdio.h>
+#include <math.h>
 
 /* Statically define the arrays a, b, and mult, where mult will become the cross product of a and b, i.e., a x b.*/
 static int a[AMAX][AMAX], b[AMAX][AMAX], mult[AMAX][AMAX];
+static double hit = 0, miss = 0, read = 0, write = 0;
+/*static int cache[2][TAGLEN];*/
 
-/* This function gets called with each "read" reference to memory */
+/*This function gets called with each "read" reference to memory */
 void mem_read(int *mp)
 {
+   int i, j;
+   read++;
+   for (i = 0; i < TAGLEN / ASSOC; i += ASSOC)
+   for (j = 0; j < ASSOC; j += 1)
+   {
+
+   }
    /* printf("Memory read from location %p\n", mp);  */
 }
-
 
 /* This function gets called with each "write" reference to memory */
 void mem_write(int *mp)
 {
-
+   write++;
    /* printf("Memory write to location %p\n", mp); */
-
 }
 
 void matmul(r1, c1, c2)
@@ -35,21 +48,20 @@ void matmul(r1, c1, c2)
    }
 
    /* Multiplying matrix a and b and storing in array mult. */
-
    for(i=0; i<r1; ++i)
    for(j=0; j<c2; ++j)
    for(k=0; k<c1; ++k)
    {
-
       #if CACHESIM		/* "Hooks" to measure memory references - enabled if CACHESIM  */
 
       mp1 = &mult[i][j];
    	mp2 = &a[i][k];
-	   mp3 = &b[k][j];   
-	   mem_read(mp1);
+	   mp3 = &b[k][j];
+      fprintf(stderr, "mp1: %p, mp2: %p, mp3: %p\n", (void *)mp1, (void *)mp2, (void *)mp3);
+	   /*mem_read(mp1);
  	   mem_read(mp2);
 	   mem_read(mp3);
-	   mem_write(mp1); 
+	   mem_write(mp1);*/
       #endif
 
       mult[i][j]+=a[i][k]*b[k][j];
@@ -60,6 +72,8 @@ int main(int argc, char **argv)
 {
    int r1, c1, r2, c2, i, j/*, k*/;
    int *mp1/*, *mp2, *mp3*/;
+
+   printf("Size of int is: %lu\n\n", sizeof(int));
 
    printf("Size of pointer is: %lu\n\n", sizeof(mp1));
 
@@ -79,7 +93,6 @@ int main(int argc, char **argv)
    }
 
    /* Storing elements of first matrix. */
-
    printf("\nEnter elements of matrix 1:\n");
    for(i=0; i<r1; ++i)
    for(j=0; j<c1; ++j)
@@ -112,5 +125,8 @@ int main(int argc, char **argv)
       if(j == c2-1)
          printf("\n\n");
    }
+
+   fprintf(stderr, "Read/Write Ratio: %f\n", read / write);
+   fprintf(stderr, "Hit Rate: %f\n", (hit * 100) / (hit + miss));
    return 0;
 }
